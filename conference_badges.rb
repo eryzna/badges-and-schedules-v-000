f72e6a9a494f8ca3624b6e30996bfb9aab3131f8
Elana Ryznar <eryzna@gmail.com>
2018-05-06 19:57:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,6 @@ end
 
 def assign_rooms (attendees)
   array=[]
-  attendees.each_with_index {|attendee, index| array << "Hello #{attendee}! You'll be assigned to room #{index+1}!"}
+  attendees.each_with_index {|attendee, index| array << "Hello, #{attendee}! You'll be assigned to room #{index+1}!"}
   array
 end
